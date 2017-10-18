534226be7e6d1f51219e8514b3004f4726c255b0
shakram02 <ahmedhamdyau@gmail.com>
2017-10-18 22:21:42 +0200

    add offset for repetition

--- src/shaders/vertex_shader.glsl
+++ src/shaders/vertex_shader.glsl
@@ -1,10 +1,11 @@
 #version 330
 
 in vec2 vert;
+in vec2 in_pos;
 in vec3 rgb_color;
 out vec3 v_color;
 
 void main(){
-    gl_Position = vec4(vert,0.0,1.0);
+    gl_Position = vec4(in_pos + vert,0.0,1.0);
     v_color = rgb_color;
 }
