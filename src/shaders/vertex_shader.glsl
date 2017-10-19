7fed11acba38248d89de7400b171f4f00ca05399
shakram02 <ahmedhamdyau@gmail.com>
2017-10-19 10:11:33 +0200

    use 3-d points

--- src/shaders/vertex_shader.glsl
+++ src/shaders/vertex_shader.glsl
@@ -1,11 +1,12 @@
 #version 330
 
-in vec2 vert;
-in vec2 in_pos;
+in vec3 vert;
+uniform vec4 MVP;
+in vec3 in_pos;
 in vec3 rgb_color;
 out vec3 v_color;
 
 void main(){
-    gl_Position = vec4(in_pos + vert,0.0,1.0);
+    gl_Position =  vec4(in_pos + vert,1.0);
     v_color = rgb_color;
 }
