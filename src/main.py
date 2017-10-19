8581666a286177b0be21023c2beea62e6547d118
shakram02 <ahmedhamdyau@gmail.com>
2017-10-19 11:12:08 +0200

    Update main
    - provide default example

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-from src.helpers.gl_program import GlProgram
+from src.helpers.gl_program_static import GlProgram
 from src.helpers.shape_helpers import Rectangle
 from src.helpers.window_creator import run_program
 
 
-def add_shapes():
-    r = Rectangle([0.3, 0.5], 0.33, 0.33, 0.0)
-    r.fill_color(0.4, 0.0, 1.0)
-
+def shape_loader():
+    r = Rectangle([0.0, 0.0], 0.4, 0.1, 0.0)
     return [r]
 
 
-if __name__ == "__main__":
-    p = GlProgram(add_shapes)
+def main():
+    p = GlProgram(shape_loader)
     run_program(p, size=(720, 328), title="Check")
+
+
+if __name__ == "__main__":
+    main()
