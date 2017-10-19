d91c0779b36a2dfbf9b18b0858d01ca79940fb64
shakram02 <ahmedhamdyau@gmail.com>
2017-10-19 11:49:00 +0200

    draw outer rectangle pattern

--- src/drawings/multi_rect_static.py
+++ src/drawings/multi_rect_static.py
@@ -1,16 +1,35 @@
-from src.helpers.gl_program_static import GlProgram
-from src.helpers.shape_helpers import Rectangle
 import src.main as m
+from src.helpers.gl_program_static import GlProgram
+from src.helpers.misc import repeat_on_quadrants
+from src.helpers.shape_helpers import Rectangle
+
+
+def symmertically_distributed_rectangles(x_origin, y_origin, rect_width, rect_height):
+    """
+    Draws 4 rectangles in 4 quadrants given their data
+    :param x_origin: x origin coordinate of rectangle
+    :param y_origin: y origin coordinate of rectangle
+    :param rect_width: width of the drawn rectangle
+    :param rect_height: height of the drawn rectangle
+    :return: list containing four rectangles distributed in the quadrants
+    """
+    rects = []
+    for x, y in repeat_on_quadrants(x_origin, y_origin):
+        r = Rectangle([x, y], rect_width, rect_height, 0.0)
+        r.fill_color(0, 1, 0.0)
+        rects.append(r)
+
+    return rects
 
 
 def add_shapes():
-    r = Rectangle([0.3, 0.5], 0.33, 0.33, 0.0)
-    r.fill_color(0.4, 0.0, 1.0)
-
-    r1 = Rectangle([0.5, 0.55], 0.33, 0.33, 0.1)
-    r1.fill_color(0.4, 0.0, 0.0)
-
-    return [r, r1]
+    rect_width = 0.03
+    rect_height = 0.03
+    rects = []
+    rects += symmertically_distributed_rectangles(0.23, 0.23, rect_width, rect_height)
+    rects += symmertically_distributed_rectangles(0.63, 0.08, rect_width, rect_height)
+    rects += symmertically_distributed_rectangles(0.08, 0.63, rect_width, rect_height)
+    return rects
 
 
 if __name__ == "__main__":
